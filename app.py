a07c40ebb8a4be82fb8527d40a85f63b6dc798ac
wkratos77 <eljadidatime@gmail.com>
2025-08-25 16:12:28 +0100

    feat

---
+++ app.py
@@ -0,0 +1,34 @@
+import os
+from flask import Flask, jsonify
+from dotenv import load_dotenv
+from config import Config
+from extensions import db, login_manager
+from routes import auth_bp, jobs_bp
+from models import User  # so db.create_all sees it
+
+def create_app():
+    load_dotenv()
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    login_manager.init_app(app)
+
+    app.register_blueprint(auth_bp, url_prefix="/auth")
+    app.register_blueprint(jobs_bp, url_prefix="/jobs")
+
+    @app.get("/")
+    def home():
+        return "Hello from JobTrackr 👋"
+
+    @app.get("/healthz")
+    def healthz():
+        return jsonify(ok=True)
+
+    return app
+
+app = create_app()
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
---
+++ routes/__init__.py
@@ -0,0 +1,4 @@
+from flask import Blueprint
+
+auth_bp = Blueprint("auth", __name__, template_folder="../templates")
+jobs_bp = Blueprint("jobs", __name__, template_folder="../templates")
